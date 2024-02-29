cfcbecdc2c9783b501430811e46e632e993beb56
Nilla Putri Rosidania <nillaputrirosidania@gmail.com>
2024-02-29 17:27:16 +0700

    first commit

---
+++ MyIntentApp_MD30NillautriRosidania/app/src/main/java/com/example/myintentapp_md30nillautrirosidania/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myintentapp_md30nillautrirosidania
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
